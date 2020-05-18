3bd2a6ddf33d726078f37b6a6ff77316ea4a77fb
ShweSinHtay <shwesin@isgm2.com>
2020-05-18 17:29:45 +0630

    Create author list page(Assignment 3: Display List of Authors)

---
+++ src/main/java/com/example/demo/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.repositories.AuthorRepository;
+@Controller
+public class AuthorController {
+private final AuthorRepository authorRepository;
+	
+	
+
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+}
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		model.addAttribute("authors",authorRepository.findAll());
+		
+		
+		return "author_list";
+	}
+
+}
